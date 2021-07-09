413c3632a73469205173362ac52de6536396b216
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-07-09 11:13:50 -0500

    add the method isValid

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,31 @@
 const validator = {
-  // ...
+  
+  isValid: function(userInput) { 
+    const userInputArray = userInput.split('');
+    const reversedUserInputArray = userInputArray.reverse().map(n => Number(n)); 
+
+    for (let i = 0; i < reversedUserInputArray.length; i++){
+      if (i % 2 !== 0) {
+        reversedUserInputArray[i] *= 2; 
+        if (reversedUserInputArray[i] >= 10){
+          reversedUserInputArray[i] -= 9;
+        }
+      }
+    }
+
+    let result = 0;
+    for (let i = 0; i < reversedUserInputArray.length; i++){
+      result += reversedUserInputArray[i]; 
+    }
+    
+    return result % 10 == 0;
+    
+  }
+    
+  
+  /*function maskify() {
+
+  } */
 };
 
 export default validator;
