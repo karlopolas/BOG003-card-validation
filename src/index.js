7b9608e49d705ce944936e520b62e538b15e8db9
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-07-09 21:39:47 -0500

    add restrictions for input field

--- src/index.js
+++ src/index.js
@@ -4,28 +4,38 @@ console.log(validator);
 
 const userInput = document.getElementById("user-input");
 const numberPlaceholder = document.getElementById("card-number");
+const inputRequired = document.getElementById('input-required')
 const btnValidate = document.getElementById("validate-btn");
 const validCardMessage = document.getElementById("valid-card");
 const invalidCardMessage = document.getElementById("invalid-card");
 
 
 userInput.addEventListener('keyup', displayCardNumber);
-btnValidate.addEventListener('click', function(){
-  const coveredNumbers = validator.maskify(numberPlaceholder.innerText);
-  const cardIsValid = validator.isValid(userInput.value);
 
-  if (cardIsValid){
-    numberPlaceholder.innerText = coveredNumbers;
-    showValidCardMessage()
-    userInput.value = '';
-  }else{
-    showinValidCardMessage()
-  }
+btnValidate.addEventListener('click', function(){
+  if(userInput.value === ''){
+    inputRequired.classList.add('show');
+  } else {
+      const coveredNumbers = validator.maskify(numberPlaceholder.innerText);
+      const cardIsValid = validator.isValid(userInput.value);
+      if (cardIsValid){
+        numberPlaceholder.innerText = coveredNumbers;
+        showValidCardMessage()
+        userInput.value = '';
+      } else{
+        showinValidCardMessage()
+        }
+    }
 });
 
 function displayCardNumber(event) {
+  inputRequired.classList.remove('show');
   const cardNumber = event.target.value;
-  numberPlaceholder.innerText = cardNumber;
+  userInput.value = cardNumber.replace(/\s/g, '').replace(/\D/g, '');
+  numberPlaceholder.innerText = userInput.value;
+  if (userInput.value == ''){
+    numberPlaceholder.innerText = '#### #### #### ####';
+  }
 }
 
 function showValidCardMessage(){
